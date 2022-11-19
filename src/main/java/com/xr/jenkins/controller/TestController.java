cbf005e50300e785e5eab4bfa8079ee73142d223
86150 <1294032118@qq.com>
2022-11-19 16:25:27 +0800

    测试提交

---
+++ src/main/java/com/xr/jenkins/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.xr.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author richard
+ * @date 2022/11/19 11:11
+ **/
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public  String test(){
+        return  "你好，我是小荣";
+    }
+}
